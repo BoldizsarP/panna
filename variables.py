96c383780cabd3a198cbe5c7846fd56a597323da
Boldizsar Pal <palboldizsar@gmail.com>
2024-04-05 15:41:31 +0200

    cucc

---
+++ variables.py
@@ -0,0 +1,15 @@
+a = "we code in python"
+b= "ew deco no pytohn"
+c ="asdasd    "
+# print(a==b)
+# print(c==c.strip())
+
+# print("Nearly the same")
+# print(c)
+# print(c.strip())
+
+# For valami in lista, avagy valami vegye fel egyszer a lista osszes elemet
+# es fusson le a belso kod
+for character in a:
+    # belso kod
+    print(character)
